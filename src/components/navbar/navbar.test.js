5659052548fe2f8b25fa3f2e16b28e0e7f4886f8
samuelkurdz <samuel@pledre.com>
2020-05-16 17:31:55 +0100

    navbar test written

--- src/components/navbar/navbar.test.js
+++ src/components/navbar/navbar.test.js
@@ -1,43 +1,9 @@
-// export default className NavbarComponent extends React.Component {
-//   // constructor(props) {
-//   //   super(props);
+import React from 'react';
+import { render } from '@testing-library/react';
+import NavBarComponent from './navbar.jsx'
 
-//   //   this.toggle = this.toggle.bind(this);
-//   //   this.state = {
-//   //     isOpen: false
-//   //   };
-//   // }
-
-//   // toggle() {
-//   //   this.setState({
-//   //     isOpen: !this.state.isOpen
-//   //   });
-//   // }
-
-//   render() {
-//     return <Fragment>
-      // <div className="page-wrapper">
-      //   <div className="nav-wrapper">
-      //     <div className="grad-bar"></div>
-      //     <nav className="navbar">
-      //       <img src="https://upload.wikimedia.org/wikipedia/en/thumb/c/c8/Bluestar_%28bus_company%29_logo.svg/1280px-Bluestar_%28bus_company%29_logo.svg.png" alt="Company Logo"/>
-      //       <div className="menu-toggle" id="mobile-menu">
-      //         <span className="bar"></span>
-      //         <span className="bar"></span>
-      //         <span className="bar"></span>
-      //       </div>
-      //       <ul className="nav no-search">
-      //         <li className="nav-item"><a href="#">Home</a></li>
-      //         <li className="nav-item"><a href="#">About</a></li>
-      //         <li className="nav-item"><a href="#">Work</a></li>
-      //         <li className="nav-item"><a href="#">Careers</a></li>
-      //         <li className="nav-item"><a href="#">Contact Us</a></li>
-      //         {/* <i className="fas fa-search" id="search-icon"></i>
-      //         <input className="search-input" type="text" placeholder="Search.."> */}
-      //       </ul>
-      //     </nav>
-      //   </div>
-      // </div>
-//     </Fragment>
-//   }
-// }
+test('renders learn react link', () => {
+  const { getByText } = render(<NavBarComponent />);
+  const linkElement = getByText(/Home/gi);
+  expect(linkElement).toBeInTheDocument();
+});
